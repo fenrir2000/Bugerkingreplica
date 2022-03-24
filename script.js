88b197a40f2f8f54509664bf53b4db850f3129d6
fenrir2000 <47163040+fenrir2000@users.noreply.github.com>
2022-03-24 14:49:11 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function MoveBars(x) {
+  x.classList.toggle("change");
+}
+
+function ImgGrid() {
+    location.href = 'https://www.google.co.uk';
+}
+
+function BottomBar() {
+    location.href = 'https://www.google.co.uk';
+}
